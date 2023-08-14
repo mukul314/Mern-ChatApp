566bab9193e64af1b6b6aa55ddc05e0f918956df
mukul314 <mukulsharma3779@gmail.com>
2023-08-15 00:02:05 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
-const path = require("path");
+// const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -39,19 +39,19 @@ app.use(errorHandler);
 
 //=========DEPLOYMENT============
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname1, 'frontend', 'build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, 'frontend', 'build')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running..');
-//   });
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..');
+  });
+}
 
 
 //=========DEPLOYMENT============
